420ba58040d3701a73ef71acd53227647f38b1dc
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2020-08-10 20:28:08 +0900

    Improve DelayFx

--- DigitalSynthVRA8Q/delay-fx.h
+++ DigitalSynthVRA8Q/delay-fx.h
@@ -1,34 +1,32 @@
 #pragma once
 
 #include "common.h"
-//#include <stdio.h>
 
 template <uint8_t T>
 class DelayFx {
-  static const uint8_t TODO = 0;
+  static const uint16_t DELAY_BUFF_SIZE = 1024;
 
-  static uint8_t  m_buff[1024];
+  static uint8_t  m_buff[DELAY_BUFF_SIZE];
   static uint16_t m_wp;
 
 public:
   INLINE static void initialize() {
-    for (uint16_t i = 0; i < 1024; ++i) {
+    for (uint16_t i = 0; i < DELAY_BUFF_SIZE; ++i) {
       m_buff[i] = 0;
     }
-    m_wp = 1023;
+    m_wp = DELAY_BUFF_SIZE - 1;
   }
 
   INLINE static void push(int8_t audio_input) {
-    m_wp = (m_wp + 1) & 0x03FF;
+    m_wp = (m_wp + 1) & (DELAY_BUFF_SIZE - 1);
     m_buff[m_wp] = audio_input;
-//printf("%u\n",m_wp);
   }
 
   INLINE static int8_t get(uint16_t sample_delay) {
-    uint16_t rp = (m_wp - sample_delay) & 0x03FF;
+    uint16_t rp = (m_wp - sample_delay) & (DELAY_BUFF_SIZE - 1);
     return m_buff[rp];
   }
 };
 
-template <uint8_t T> uint8_t  DelayFx<T>::m_buff[1024];
+template <uint8_t T> uint8_t  DelayFx<T>::m_buff[DELAY_BUFF_SIZE];
 template <uint8_t T> uint16_t DelayFx<T>::m_wp;
