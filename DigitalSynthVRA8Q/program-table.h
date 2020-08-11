1aabb3049f1c482c48b3d800b3a6bab55752447a
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2020-08-11 21:36:37 +0900

    Change PRESETs

--- DigitalSynthVRA8Q/program-table.h
+++ DigitalSynthVRA8Q/program-table.h
@@ -6,14 +6,14 @@ const uint8_t PROGRAM_NUMBER_DEFAULT = 0;
 const uint8_t g_preset_table_OSC_WAVE       [] = {0  , 0  , 0  , 127, 127, 0  , 0  , 0  };
 const uint8_t g_preset_table_EG_TO_PITCH    [] = {64 , 64 , 64 , 64 , 64 , 64 , 64 , 64 };
 
-const uint8_t g_preset_table_CUTOFF         [] = {64 , 16 , 16 , 112, 0  , 100, 64 , 127};
-const uint8_t g_preset_table_RESONANCE      [] = {96 , 80 , 127, 0  , 0  , 64 , 64 , 0  };
-const uint8_t g_preset_table_EG_TO_CUTOFF   [] = {82 , 112, 94 , 64 , 64 , 64 , 64 , 64 };
-const uint8_t g_preset_table_AMP_EG         [] = {0  , 0  , 127, 0  , 127, 127, 127, 127};
+const uint8_t g_preset_table_CUTOFF         [] = {127, 16 , 16 , 112, 0  , 100, 64 , 127};
+const uint8_t g_preset_table_RESONANCE      [] = {0  , 80 , 127, 0  , 0  , 64 , 96 , 0  };
+const uint8_t g_preset_table_EG_TO_CUTOFF   [] = {64 , 112, 94 , 64 , 64 , 64 , 82 , 64 };
+const uint8_t g_preset_table_AMP_EG         [] = {127, 0  , 127, 0  , 127, 127, 0  , 127};
 
 const uint8_t g_preset_table_ATTACK         [] = {0  , 0  , 0  , 0  , 32 , 16 , 0  , 0  };
-const uint8_t g_preset_table_DECAY          [] = {96 , 96 , 96 , 96 , 32 , 32 , 64 , 0  };
-const uint8_t g_preset_table_SUSTAIN        [] = {0  , 0  , 0  , 0  , 127, 96 , 0  , 127};
+const uint8_t g_preset_table_DECAY          [] = {0  , 96 , 96 , 96 , 32 , 32 , 96 , 0  };
+const uint8_t g_preset_table_SUSTAIN        [] = {127, 0  , 0  , 0  , 127, 96 , 0  , 127};
 
 const uint8_t g_preset_table_LFO_DEPTH      [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
 const uint8_t g_preset_table_LFO_RATE       [] = {64 , 64 , 64 , 64 , 64 , 64 , 64 , 64 };
