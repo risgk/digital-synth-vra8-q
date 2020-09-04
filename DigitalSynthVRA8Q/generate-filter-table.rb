94f0d8d17a539380d249e2604a2c9259f392dace
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2020-09-04 20:02:02 +0900

    Fix filter-table

--- DigitalSynthVRA8Q/generate-filter-table.rb
+++ DigitalSynthVRA8Q/generate-filter-table.rb
@@ -38,7 +38,7 @@ def generate_filter_lpf_table(name, q)
   $file.printf("};\n\n")
 end
 
-(0..5).each do |idx|
+(0..7).each do |idx|
   generate_filter_lpf_table(idx.to_s, Math.sqrt(2.0) ** (idx - 1.0))
 end
 
