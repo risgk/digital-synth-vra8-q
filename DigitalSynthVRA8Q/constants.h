f1d8e350131d04c81f406698c80acb761f8decd0
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2020-12-07 23:16:53 +0900

    Improve sound quality

--- DigitalSynthVRA8Q/constants.h
+++ DigitalSynthVRA8Q/constants.h
@@ -4,7 +4,7 @@ const double    A4_PITCH = 440.0;
 
 const uint8_t   MIDI_CH             = 0;
 const uint16_t  SAMPLING_RATE       = 31250;
-const uint16_t  FREQUENCY_MAX       = 14000;
+const uint16_t  FREQUENCY_MAX       = 14500;
 const uint8_t   BIT_DEPTH           = 8;
 const uint8_t   NOTE_NUMBER_MIN     = 24;
 const uint8_t   NOTE_NUMBER_MAX     = 96;
