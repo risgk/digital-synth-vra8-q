06120b4416cf477b3569ee0e3b0d747a6acc662e
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2020-12-29 21:51:48 +0900

    Change a level of g_osc_sin_wave_table_h1

--- DigitalSynthVRA8Q/generate-osc-table.rb
+++ DigitalSynthVRA8Q/generate-osc-table.rb
@@ -125,7 +125,7 @@ generate_osc_wave_table_arrays do |last|
   end
 end
 
-generate_osc_wave_table("sin", 1, Math.sqrt(2.0) / Math.sqrt(3.0)) do |n, k|
+generate_osc_wave_table("sin", 1, Math.sqrt(2.0) * (Math.sqrt(2.0) / Math.sqrt(3.0))) do |n, k|
   Math.sin((2.0 * Math::PI) * (n.to_f / (1 << OSC_WAVE_TABLE_SAMPLES_BITS)) * k)
 end
 
