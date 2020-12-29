e37762d6eccc264a1b9f54dfb64966690b0ca8ef
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2020-12-29 23:22:30 +0900

    Delete g_osc_sin_wave_table_h1

--- DigitalSynthVRA8Q/generate-osc-table.rb
+++ DigitalSynthVRA8Q/generate-osc-table.rb
@@ -125,10 +125,6 @@ generate_osc_wave_table_arrays do |last|
   end
 end
 
-generate_osc_wave_table("sin", 1, Math.sqrt(2.0) * (Math.sqrt(2.0) / Math.sqrt(3.0))) do |n, k|
-  Math.sin((2.0 * Math::PI) * (n.to_f / (1 << OSC_WAVE_TABLE_SAMPLES_BITS)) * k)
-end
-
 def generate_osc_wave_tables_array(name, organ = false, organ_last = 8)
   $file.printf("const uint8_t* const g_osc_#{name}_wave_tables[] PROGMEM = {\n  ")
   $osc_harmonics_restriction_table.each_with_index do |freq, idx|
