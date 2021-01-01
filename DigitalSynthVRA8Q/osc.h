f312738f6f23068f854e51c0ede4e8e836b9dc79
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2021-01-01 15:04:41 +0900

    Change the attack time of Gate

--- DigitalSynthVRA8Q/osc.h
+++ DigitalSynthVRA8Q/osc.h
@@ -462,16 +462,18 @@ private:
 
   template <uint8_t N>
   INLINE static void update_gate() {
-    const uint8_t quarter_level = (m_osc_level >> 2) + 1;
-
     if (m_osc_on[N]) {
-      if (m_osc_gain[N] >= (m_osc_level - quarter_level)) {
+      const uint8_t half_level = (m_osc_level >> 1) + 1;
+
+      if (m_osc_gain[N] >= (m_osc_level - half_level)) {
         m_osc_gain[N] = m_osc_level;
       } else {
-        m_osc_gain[N] += quarter_level;
+        m_osc_gain[N] += half_level;
       }
     }
     else {
+      const uint8_t quarter_level = (m_osc_level >> 2) + 1;
+
       if (m_osc_gain[N] <= quarter_level) {
         m_osc_gain[N] = 0;
       } else {
