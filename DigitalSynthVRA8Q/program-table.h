70873b4ff9f40ef606b8b48af3ee6d8e4068591f
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2021-01-03 15:27:21 +0900

    Revert "Revert "Limit Filter Resonance (Max Q = 8.0)""
    This reverts commit 142cf3b7453e63f63c169fab8a808ba9f7844ea1.

--- DigitalSynthVRA8Q/program-table.h
+++ DigitalSynthVRA8Q/program-table.h
@@ -7,7 +7,7 @@ const uint8_t g_preset_table_OSC_WAVE       [] = {0  , 0  , 0  , 127, 0  , 0  , 
 const uint8_t g_preset_table_OSC_LEVEL      [] = {100, 100, 100, 100, 100, 100, 100, 100};
 
 const uint8_t g_preset_table_CUTOFF         [] = {64 , 16 , 112, 64 , 127, 127, 127, 127};
-const uint8_t g_preset_table_RESONANCE      [] = {64 , 48 , 32 , 64 , 0  , 0  , 0  , 0  };
+const uint8_t g_preset_table_RESONANCE      [] = {80 , 64 , 48 , 80 , 0  , 0  , 0  , 0  };
 const uint8_t g_preset_table_EG_TO_CUTOFF   [] = {82 , 112, 16 , 88 , 64 , 64 , 64 , 64 };
 const uint8_t g_preset_table_AMP_EG         [] = {127, 0  , 0  , 127, 127, 127, 127, 127};
 
