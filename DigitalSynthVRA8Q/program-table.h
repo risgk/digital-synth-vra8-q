42174d1a45f9fda8977532b8d07bc318e77ab9b1
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2021-01-14 23:03:34 +0900

    Change PRESET programs

--- DigitalSynthVRA8Q/program-table.h
+++ DigitalSynthVRA8Q/program-table.h
@@ -7,7 +7,7 @@ const uint8_t g_preset_table_OSC_WAVE       [] = {0  , 0  , 0  , 127, 0  , 0  , 
 const uint8_t g_preset_table_OSC_LEVEL      [] = {127, 127, 127, 127, 127, 127, 127, 127};
 
 const uint8_t g_preset_table_CUTOFF         [] = {64 , 40 , 112, 64 , 127, 127, 127, 127};
-const uint8_t g_preset_table_RESONANCE      [] = {80 , 64 , 48 , 80 , 0  , 0  , 0  , 0  };
+const uint8_t g_preset_table_RESONANCE      [] = {48 , 32 , 64 , 32 , 0  , 0  , 0  , 0  };
 const uint8_t g_preset_table_EG_TO_CUTOFF   [] = {82 , 100, 16 , 88 , 64 , 64 , 64 , 64 };
 const uint8_t g_preset_table_AMP_EG         [] = {127, 0  , 0  , 127, 127, 127, 127, 127};
 
