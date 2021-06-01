d6799facd968d8097caafdc87078ce0be7da1239
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2021-06-01 18:06:53 +0900

    Fix wave tables

--- DigitalSynthVRA8Q/generate-osc-table.rb
+++ DigitalSynthVRA8Q/generate-osc-table.rb
@@ -60,9 +60,11 @@ def generate_osc_wave_table(name, last, amp, organ = false)
   $file.printf("const uint8_t g_osc_#{name}_wave_table_h%d[] PROGMEM = {\n  ", last)
   (0..(1 << OSC_WAVE_TABLE_SAMPLES_BITS)).each do |n|
     level = 0
+    nn = n
+    nn = 0 if n == (1 << OSC_WAVE_TABLE_SAMPLES_BITS)
     max = organ ? last * 2 : last
     (1..max).each do |k|
-      level += yield(n, k)
+      level += yield(nn, k)
     end
     level *= amp
     level = (level * OSC_WAVE_TABLE_AMPLITUDE).floor.to_i
